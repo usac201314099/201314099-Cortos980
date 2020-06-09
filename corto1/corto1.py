2b40ff7b22932c1e887c7fb39160d84cb8704fc9
usac201314099 <2093932240115@ingenieria.usac.edu.gt>
2020-06-09 12:31:33 -0600

    Solo la secuencia

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -1,1 +1,26 @@
 #corto 1 
+#carne 201314099 ----099
+#congetura 3n+1
+numero=0
+
+Collatz = [] #lista para guardar la secuencias
+
+def Secuencia(numero): #funcion para hacer la secuencia
+    N=int(numero)
+    print(N)
+    while N > 1:  
+        if N%2 ==0: #comprueva se es par
+            Collatz.append(N) #guarda en la lista
+            N=N/2 #el siguiente sera la mitad
+        else:
+            Collatz.append(N) # si no es para 
+            N=3*N+1
+    Collatz.append(1) #agraga la ailtima cifra 1       
+    print(Collatz)  
+
+Secuencia(6)
+    
+    
+    
+
+    
